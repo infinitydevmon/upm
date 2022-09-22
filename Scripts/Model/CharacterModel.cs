dcaea72921896b121b38b3b4f198c2c77281bf0a
안구건조증 <szone2s@naver.com>
2022-09-22 13:15:48 +0900

    f

--- Scripts/Model/CharacterModel.cs
+++ Scripts/Model/CharacterModel.cs
@@ -1,21 +1,23 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class CharacterModel
+namespace IVSendBox.Models
 {
-    public string JsonKey;
-    public string Name;
-
-    public static List<CharacterModel> Molde = new List<CharacterModel>() { character1, character2 };
-
-    public static CharacterModel character1
+    public class CharacterModel
     {
-        get { return new CharacterModel { JsonKey = "character1", Name = "Gwangmo" }; }
-    }
+        public string JsonKey;
+        public string Name;
 
-    public static CharacterModel character2
-    {
-        get { return new CharacterModel { JsonKey = "character1", Name = "jung" }; }
+        public static List<CharacterModel> Molde = new List<CharacterModel>() { character1, character2 };
+
+        public static CharacterModel character1
+        {
+            get { return new CharacterModel { JsonKey = "character1", Name = "Gwangmo" }; }
+        }
+
+        public static CharacterModel character2
+        {
+            get { return new CharacterModel { JsonKey = "character1", Name = "jung" }; }
+        }
     }
 }
+
