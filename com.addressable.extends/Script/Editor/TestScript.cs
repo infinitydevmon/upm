45e248bd40fa27665d01ae4ca5fd2c9dbbcb83f6
안구건조증 <szone2s@naver.com>
2022-09-22 11:15:47 +0900

    f

---
+++ com.addressable.extends/Script/Editor/TestScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    static TestScript instance;
+
+    public static TestScript Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                var go = new GameObject();
+                go.transform.parent = GameObject.Find("UI Root").transform;
+                go.transform.localScale = Vector3.one;
+                go.transform.localPosition = Vector3.zero;
+                instance = go.GetComponent<TestScript>();
+            }
+
+            return instance;
+        }
+    }
+
+    public void Display()
+    {
+    }
+}
